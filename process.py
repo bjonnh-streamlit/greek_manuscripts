2318061713a76384f2122a0cb978a7be34889596
Jonathan Bisson <bjo@uic.edu>
2021-11-04 15:28:53 -0500

    Final prototype 1

--- process.py
+++ process.py
@@ -1,8 +1,12 @@
 import textract
+from cltk.lemmatize import GreekBackoffLemmatizer
+from docx import Document
+from docx.enum.section import WD_SECTION_START
+from cltk.alphabet.text_normalization import cltk_normalize
 from enum import Enum
 import re
 from collections import defaultdict
-from cltk import NLP
+from docx.shared import Pt
 
 FILE = "data/Galen Simpl Med 01 (Convert'd) Books 06-11.doc"
 
@@ -24,13 +28,13 @@ class Decoder:
         DecoderStates.IN_SECTION: [DecoderStates.IN_SUBSECTION],
         DecoderStates.IN_TEXT: []
     }
-    nlp = NLP(language='grc')
 
     def __init__(self):
         self.STATE = DecoderStates.UNKNOWN
         self.section = ""
         self.subsection = ""
         self.line_counter = 1
+        self.nlp = None
 
         # word to positions as list
         self.word_occurrences = defaultdict(list)
@@ -44,9 +48,21 @@ class Decoder:
     def current_reference(self):
         return f"{self.subsection} ({self.section}.{self.line_counter})"
 
-    def lemmatize(self, text):
-        print(text)
-        print(self.nlp.analyze(text).lemmata)
+    def lemma(self, debug=False):
+        lemmatizer = GreekBackoffLemmatizer()
+        output = defaultdict(set)
+        total = len(self.word_occurrences)
+
+        words = set()
+        for word in self.word_occurrences.keys():
+            words.add(cltk_normalize(word))
+        if debug:
+            print(f" Normalized, found {len(words)} unique words")
+        for word in words:
+            lemma = lemmatizer.lemmatize([word])[0]
+            output[lemma[1]].add(word)
+
+        return sorted(output.items())
 
     def process_textline(self, textline):
         words = textline.split()
@@ -56,7 +72,7 @@ class Decoder:
                 self.word_occurrences[cleaned_word].append(self.current_reference())
                 self.reversed_word_occurrences[cleaned_word[::-1]].append(self.current_reference())
 
-    def process_line(self, line, with_lemmatization=False):
+    def process_line(self, line):
         clean_line = line.strip()
         if clean_line == "":
             return
@@ -71,15 +87,13 @@ class Decoder:
             if subsection != "":
                 self.subsection = subsection
             self.process_textline(line_match.group(3).strip())
-            if with_lemmatization:
-                self.lemmatize(line)
         else:
             print(f"Can't handle {clean_line}")
 
     def word_info(self, word, source):
         content = source[word]
 
-        return f"{word}: ({len(content)}) {', '.join(content)}"
+        return f"({len(content)}) {', '.join(content)}"
 
     def index(self, reversed=False):
         if reversed:
@@ -88,7 +102,7 @@ class Decoder:
             source = self.word_occurrences
 
         for word in sorted(source.keys()):
-            print(decoder.word_info(word, source))
+            yield word, decoder.word_info(word, source)
 
     def count(self):
         counts = {}
@@ -98,15 +112,70 @@ class Decoder:
         return sorted(counts.items(), key=lambda kv: kv[1], reverse=True)
 
 
+WNS_COLS_NUM = "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}num"
+
+
+class DocxGenerator:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        document = Document()
+        style = document.styles["Normal"]
+        font = style.font
+        font.name = "Galatia sil"
+        font.size = Pt(10.5)
+
+        section = document.add_section(WD_SECTION_START.CONTINUOUS)
+        section._sectPr.xpath("./w:cols")[0].set(WNS_COLS_NUM, str(2))
+        self.p = document.add_paragraph()
+        self.document = document
+        return self
+
+    def write(self, text, bold=False):
+        out = self.p.add_run(text)
+        if bold:
+            out.bold = True
+
+    def __exit__(self, type, value, traceback):
+        self.document.save(self.filename)
+
+
 text = textract.process(FILE).decode('utf-8')
 
 decoder = Decoder()
 
 for line in text.splitlines():
-    decoder.process_line(line, with_lemmatization=False)
+    decoder.process_line(line)
 
 # Processing of Galien takes ~ 3s
 # Lemmatization of Galien takes ~ 22min
 
 # for it in decoder.count()[0:100]:
 #    print(it)
+
+
+print("Generating direct index")
+with DocxGenerator("out/index.docx") as generator:
+    for i in decoder.index():
+        generator.write(i[0], bold=True)
+        generator.write(f": {i[1]}\n")
+
+print("Generating inverted index")
+with DocxGenerator("out/index_inverse.docx") as generator:
+    for i in decoder.index(reversed=True):
+        generator.write(i[0], bold=True)
+        generator.write(f": {i[1]}\n")
+
+print("Generating frequency list")
+with DocxGenerator("out/frequency.docx") as generator:
+    for i in decoder.count():
+        generator.write(i[0], bold=True)
+        generator.write(f": {i[1]}\n")
+
+print("Generating lemma list")
+with DocxGenerator("out/lemma.docx") as generator:
+    generator.write("Generated with the CLTK Backoff Lematizer\n\n", bold=True)
+    for i in decoder.lemma(debug=True):
+        generator.write(i[0], bold=True)
+        generator.write(f": {', '.join(i[1])}\n")
