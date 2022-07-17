b08281d58d20dbb4bfe4164c606f1cc5fe31095c
Jonathan Bisson <bjonnh-github@bjonnh.net>
2022-07-17 18:00:15 -0500

    Streamlit version

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import os
+import io
+import time
 from zipfile import ZipFile
+import tempfile
 import sys
 
 import textract
@@ -8,61 +11,90 @@ from lib.decoder import Decoder
 from lib.docxgenerator import DocxGenerator
 from lib.logger import Logger
 
-FILE = sys.argv[1]
-OUT = sys.argv[2]
+import streamlit as st
+
 # Limits the output files size so they are easier to look through
 DEBUG = False
 
-full_text = textract.process(FILE).decode('utf-8')
 
-logger = Logger()
-logger.set_hook(lambda logentry: print(logentry))
-decoder = Decoder(logger)
+def processor(file, original_file_name, out):
+    output_zip = io.BytesIO()
+    full_text = textract.process(file, extension="doc").decode('utf-8')
 
-count = 100
+    log_file = open(f"{out}/log.txt", "w")
+    logger = Logger()
 
-for line in full_text.splitlines():
-    decoder.process_line(line)
-    if DEBUG:
-        count -= 1
-        if count == 0:
-            break
+    def write_log(logentry):
+        log_file.write(logentry.__str__())
 
-os.makedirs("out", exist_ok=True)
-logger.info(f"Analyzing {FILE}")
-logger.info("Generating direct index")
-with DocxGenerator(f"{OUT}/index.docx") as generator:
-    for i in decoder.index():
-        generator.write(i[0], bold=True)
-        generator.write(f": {i[1]}")
-        generator.new_paragraph()
+    logger.set_hook(lambda logentry: write_log(logentry))
+    decoder = Decoder(logger)
 
-logger.info("Generating inverted index")
-with DocxGenerator(f"{OUT}/index_inverse.docx") as generator:
-    for i in decoder.index(reverse=True):
-        generator.write(i[0], bold=True)
-        generator.write(f": {i[1]}")
-        generator.new_paragraph()
+    count = 100
 
-logger.info("Generating frequency list")
-with DocxGenerator(f"{OUT}/frequency.docx") as generator:
-    for i in decoder.count():
-        generator.write(i[0], bold=True)
-        generator.write(f": {i[1]}")
-        generator.new_paragraph()
+    for line in full_text.splitlines():
+        decoder.process_line(line)
+        if DEBUG:
+            count -= 1
+            if count == 0:
+                break
 
-logger.info("Generating lemma list")
-with DocxGenerator(f"{OUT}/lemma.docx") as generator:
-    for i in decoder.lemma(debug=True):
-        generator.write(i[0], bold=True)
-        generator.write(f": {', '.join(i[1])}")
-        generator.new_paragraph()
+    logger.info(f"Analyzing {original_file_name}")
+    logger.info("Generating direct index")
+    with DocxGenerator(f"{out}/index.docx") as generator:
+        for i in decoder.index():
+            generator.write(i[0], bold=True)
+            generator.write(f": {i[1]}")
+            generator.new_paragraph()
 
-ZIPFILENAME = OUT + "/" + os.path.split(os.path.splitext(FILE)[0])[-1] + ".zip"
+    logger.info("Generating inverted index")
+    with DocxGenerator(f"{out}/index_inverse.docx") as generator:
+        for i in decoder.index(reverse=True):
+            generator.write(i[0], bold=True)
+            generator.write(f": {i[1]}")
+            generator.new_paragraph()
 
-with ZipFile(ZIPFILENAME, "w") as myzip:
-    myzip.write(f"{OUT}/index.docx", arcname="index.docx")
-    myzip.write(f"{OUT}/index_inverse.docx", arcname="index_inverse.docx")
-    myzip.write(f"{OUT}/frequency.docx", arcname="frequency.docx")
-    myzip.write(f"{OUT}/lemma.docx", arcname="lemma.docx")
-    myzip.write(FILE, arcname=os.path.basename(FILE))
+    logger.info("Generating frequency list")
+    with DocxGenerator(f"{out}/frequency.docx") as generator:
+        for i in decoder.count():
+            generator.write(i[0], bold=True)
+            generator.write(f": {i[1]}")
+            generator.new_paragraph()
+
+    logger.info("Generating lemma list")
+    with DocxGenerator(f"{out}/lemma.docx") as generator:
+        for i in decoder.lemma(debug=True):
+            generator.write(i[0], bold=True)
+            generator.write(f": {', '.join(i[1])}")
+            generator.new_paragraph()
+
+    log_file.close()
+    myzip = ZipFile(output_zip, "w")
+    myzip.write(f"{out}/index.docx", arcname="index.docx")
+    myzip.write(f"{out}/index_inverse.docx", arcname="index_inverse.docx")
+    myzip.write(f"{out}/frequency.docx", arcname="frequency.docx")
+    myzip.write(f"{out}/lemma.docx", arcname="lemma.docx")
+    myzip.write(f"{out}/log.txt", arcname="log.txt")
+    myzip.write(file, arcname=original_file_name)
+
+    st.download_button("Download zip file with processed document", output_zip,
+                        file_name=original_file_name.replace("doc", "zip"))
+    messages = ""
+    for message in logger.messages:
+        messages += message.__str__() + "\n"
+    st.code(messages)
+
+
+st.title("Greek manuscript processor")
+
+
+uploaded_file = st.file_uploader("Manuscript file", type="doc", help="Please upload a manuscript file in the right format")
+
+if uploaded_file is not None:
+    def process_streamlit():
+        with tempfile.TemporaryDirectory() as out:
+            with tempfile.NamedTemporaryFile() as in_file:
+                in_file.write(uploaded_file.read())
+                in_file.flush()
+                processor(in_file.name, uploaded_file.name, out)
+    process = st.button("Process", on_click=process_streamlit)
