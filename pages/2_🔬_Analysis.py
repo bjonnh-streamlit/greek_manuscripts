131a96e61fe27778b26e230bd636a052d0c008a4
Jonathan Bisson <bjonnh-github@bjonnh.net>
2022-11-20 21:26:37 -0600

    Add clickable text and dynamic analysis

--- pages/2_🔬_Analysis.py
+++ pages/2_🔬_Analysis.py
@@ -6,6 +6,7 @@ from nltk import ngrams
 import numpy as np
 import itertools
 import pandas as pd
+import st_clickable_text
 
 import textract
 from lib.decoder import Decoder
@@ -50,12 +51,13 @@ def processor(file):
     full_text_as_list = decoder.full_text_by_section()
     logger.info(f"We have {len(full_text_as_list)} sections.")
     data = list(itertools.chain.from_iterable(full_text_as_list))
-
-    matrix, vocab_index = generate_co_occurrence_matrix(data, 8)
+    return decoder.state, data
+def get_matrix(data, distance, forward, backward):
+    matrix, vocab_index = generate_co_occurrence_matrix(data, distance, forward=forward, backward=backward)
     data_matrix = pd.DataFrame(matrix, index=vocab_index, columns=vocab_index)
     words_by_occurence = data_matrix.sum(axis=0).sort_values(ascending=False)
 
-    return decoder, words_by_occurence, data_matrix
+    return words_by_occurence, data_matrix
 
     #nlp = spacy.load("el_core_news_lg")
     # for doc in nlp.pipe(full_text): #, disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"]):
@@ -85,7 +87,11 @@ def combine_ngrams_to_repeated_couples(ngram, distance):
     return output
 
 
-def generate_co_occurrence_matrix(corpus, distance):
+@st.experimental_memo
+def generate_co_occurrence_matrix(corpus, distance, forward=True, backward=False):
+    if not forward and not backward:
+        raise Exception("You need at least one of forward or backward")
+
     vocab = set(corpus)
     vocab = list(vocab)
     vocab_index = {word: i for i, word in enumerate(vocab)}
@@ -94,10 +100,12 @@ def generate_co_occurrence_matrix(corpus, distance):
     calculated_ngrams = ngrams(corpus, distance)
     repeated_ngrams = []
 
-    for ngram in calculated_ngrams:
-        repeated_ngrams += combine_ngrams_to_repeated_couples(ngram, distance)
-    for ngram in ngrams(reversed(corpus), distance):
-        repeated_ngrams += combine_ngrams_to_repeated_couples(ngram, distance)
+    if forward:
+        for ngram in calculated_ngrams:
+            repeated_ngrams += combine_ngrams_to_repeated_couples(ngram, distance)
+    if backward:
+        for ngram in ngrams(reversed(corpus), distance):
+            repeated_ngrams += combine_ngrams_to_repeated_couples(ngram, distance)
 
     # Frequency distribution of ngrams ((word1, word2), num_occurrences)
     ngram_freq = nltk.FreqDist(repeated_ngrams).most_common(len(repeated_ngrams))
@@ -132,35 +140,52 @@ def process_data(uploaded_file_name):
     with tempfile.NamedTemporaryFile() as in_file:
         in_file.write(uploaded_file.read())
         in_file.flush()
-        decoder, words_by_occurence, data_matrix = processor(in_file.name)
-        return decoder.state, words_by_occurence, data_matrix
+        decoder_state, data = processor(in_file.name)
+    return decoder_state, data
 
 
 if uploaded_file is not None:
     st.session_state['uploaded_file'] = uploaded_file.name
 
-    st.write("Uploaded file:", uploaded_file.name)
-
-    decoder_state, words_by_occurence, data_matrix = process_data(uploaded_file.name)
+    decoder_state, data = process_data(uploaded_file.name)
     sections = list(decoder_state.sections)
     sections.sort()
 
-    selected_section = st.selectbox('Section', sections)
-    subsections = list(decoder_state.subsections[selected_section])
-    subsections.sort()
-    selected_subsection = st.selectbox('Subsection', subsections)
+    col1, col2 = st.columns(2)
+
+    with col1:
+        selected_section = st.selectbox('Section', sections)
+    with col2:
+        subsections = list(decoder_state.subsections[selected_section])
+        subsections.sort()
+        selected_subsection = st.selectbox('Subsection', subsections)
 
     text = decoder_state.full_text[Reference(selected_section, selected_subsection)]
 
     st.subheader("Text")
-    st.write(" ".join(text))
-    st.subheader("Top 10 words")
-    st.dataframe(words_by_occurence)
-    st.subheader("Word lookup")
-    word_lookup = st.text_input("Word to lookup", "")
-    if word_lookup != '' and word_lookup in data_matrix.index:
-        top10 = data_matrix[word_lookup].sort_values(ascending=False)[0:10]
-        st.write(f"Top 10 words related to {word_lookup}")
-        st.dataframe(top10[top10 > 0])
+    st.text("Click on a word to get more information about it")
+    clicked_word_index = st_clickable_text.my_component(text)
 
+    if (clicked_word_index > -1) and (clicked_word_index < len(text)):
+        word_lookup = text[clicked_word_index]
+    else:
+        word_lookup = ""
 
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        distance = st.slider("Distance in words", min_value=1, max_value=20, value=8)
+    with col2:
+        forward = st.checkbox("Forward matching", value=True)
+    with col3:
+        backward = st.checkbox("Backward matching")
+
+    if (not forward) and (not backward):
+        st.error("Sorry you need to select at least one or both of Forward and backward")
+    else:
+        words_by_occurence, data_matrix = get_matrix(data, distance=distance, forward=forward, backward=backward)
+
+        if word_lookup != '' and word_lookup in data_matrix.index:
+            top10 = data_matrix[word_lookup].sort_values(ascending=False)[0:10]
+            st.subheader(f"Top 10 words related to {word_lookup} (in the whole document)")
+            x = pd.DataFrame(top10[top10 > 0]).transpose()
+            st.dataframe(x)
---
+++ st_clickable_text/frontend/build/precache-manifest.a7fc06339c165583252c85a3fb4cf37a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8473193e751a13c496f013cdf02024",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c5b554c54e8e0432440",
+    "url": "./static/js/2.53d8fbe5.chunk.js"
+  },
+  {
+    "revision": "465ff09073ee359151abc124f9b2a2ea",
+    "url": "./static/js/2.53d8fbe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "664356773f0cd9f1af7f",
+    "url": "./static/js/main.f3be604c.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
